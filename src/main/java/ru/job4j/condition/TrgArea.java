8dd4f89a3cf09073dd63dced735016100463c05b
andre <andreyklimenko@list.ru>
2023-10-01 00:08:09 +0300

    3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/condition/TrgArea.java
+++ src/main/java/ru/job4j/condition/TrgArea.java
@@ -4,9 +4,8 @@ public class TrgArea {
     public static double area(double a, double b, double c) {
         double p = (a + b + c) / 2;
         double s = p * (p - a) * (p - b) * (p - c);
-        double rsl = Math.sqrt(s);
-
-        return rsl;
+        double three = Math.sqrt(s);
+        return three;
     }
 
     public static void main(String[] args) {
