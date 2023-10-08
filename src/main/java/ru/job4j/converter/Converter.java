c619b463d4400ebf7433129309f925583df4b3dd
andre <andreyklimenko@list.ru>
2023-10-08 19:42:58 +0300

    3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -13,17 +13,12 @@ public class Converter {
     }
 
     public static void main(String[] args) {
-        float in = 140;
-        float expected = 2;
-        float out = Converter.rubleToEuro(in);
+        float in = 180;
+        float expected = 3;
+        float out = Converter.rubleToDollar(in);
         boolean passed = expected == out;
-        System.out.println("140 rubles are 2. Test result : " + passed);
-        float in2 = 180;
-        float expected2 = 3;
-        float out2 = Converter.rubleToDollar(in2);
-        boolean passed2 = expected2 == out2;
-        System.out.println("180 rubles are 3. Test result : " + passed2);
-    }
+        System.out.println("180 rubles are 3. Test result : " + passed);
+            }
 
     }
 
