1bd988b132acc14470600c1a4e7508149a5e7382
andre <andreyklimenko@list.ru>
2023-10-16 17:16:03 +0300

    3.3. Фиксация изменений на github [#243564]

---
+++ src/test/java/ru/job4j/PointTest.java
@@ -0,0 +1,58 @@
+package ru.job4j;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class PointTest {
+    @Test
+    void when00to20then2() {
+        double expected = 2;
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 2;
+        int y2 = 0;
+        double out = Point.distance(x1, y1, x2, y2);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void when00to55then7() {
+        double expected =7.07;
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 5;
+        int y2 = 5;
+        double out = Point.distance(x1, y1, x2, y2);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+    @Test
+    void when00to64then7() {
+        double expected =7.21;
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 6;
+        int y2 = 4;
+        double out = Point.distance(x1, y1, x2, y2);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+    @Test
+    void when18to25then3() {
+        double expected =3.16;
+        int x1 = 1;
+        int y1 = 8;
+        int x2 = 2;
+        int y2 = 5;
+        double out = Point.distance(x1, y1, x2, y2);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+    @Test
+    void when52to43then1() {
+        double expected =1.41;
+        int x1 = 5;
+        int y1 = 2;
+        int x2 = 4;
+        int y2 = 3;
+        double out = Point.distance(x1, y1, x2, y2);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+}
