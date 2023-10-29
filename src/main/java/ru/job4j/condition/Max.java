4bd363d31a2d56ec9e69b5472d70fa371e00c68f
andre <andreyklimenko@list.ru>
2023-10-29 23:28:09 +0300

    3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -2,13 +2,11 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int left, int right) {
-        boolean condition = true;
-        int result = condition ? left : right;
-        return result;
+        return left > right ? left : right;
     }
 
     public static void main(String[] args) {
-        int result  = Max.max(1, 1);
+        int result = Max.max(1, 1);
         System.out.println(result);
-}
+    }
 }
