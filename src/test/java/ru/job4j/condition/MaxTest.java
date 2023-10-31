a7ae918ad73a28e55970f2c1261c4a0cb5db2e66
andre <andreyklimenko@list.ru>
2023-10-31 23:20:18 +0300

    3.3. Фиксация изменений на github [#243564]

--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -6,10 +6,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 class MaxTest {
     @Test
     void whenMax1To2Then2() {
-        int left = 1;
-        int right = 2;
+        int left = 2;
+        int right = 1;
         int result = Max.max(left, right);
-        int expected = 1;
+        int expected = 2;
         assertThat(result).isEqualTo(expected);
     }
 
