d5fdf75c7f109ed54759cd2a945c11a0a716fa0e
andre <andreyklimenko@list.ru>
2023-11-05 22:17:51 +0300

    3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/Point.java
+++ src/main/java/ru/job4j/Point.java
@@ -10,5 +10,11 @@ public class Point {
         System.out.println("result (0, 0) to (2, 0) " + result);
         double result2 = Point.distance(0, 0, 5, 5);
         System.out.println("result (0, 0) to (5, 5) " + result2);
-    }
+        double result3 = Point.distance(0, 0, 6, 4);
+        System.out.println("result (0, 0) to (6, 4) " + result3);
+        double result4 = Point.distance(1, 8, 2, 5);
+        System.out.println("result (1, 8) to (2, 5) " + result4);
+        double result5 = Point.distance(5, 2, 4, 3);
+        System.out.println("result (5, 2) to (4, 3) " + result5);
+}
 }
