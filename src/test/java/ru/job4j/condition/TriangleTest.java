fcc5d73f5ffb08e5bd20e254a1c2a49821f7e7cb
andre <andreyklimenko@list.ru>
2023-11-05 21:28:53 +0300

    3.3. Фиксация изменений на github [#243564]

--- src/test/java/ru/job4j/condition/TriangleTest.java
+++ src/test/java/ru/job4j/condition/TriangleTest.java
@@ -1,9 +1,11 @@
 package ru.job4j.condition;
 
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class TriangleTest {
+
     @Test
     void whenExist() {
         double ab = 2.0;
@@ -12,6 +14,7 @@ class TriangleTest {
         boolean result = Triangle.exist(ab, ac, bc);
         assertThat(result).isTrue();
     }
+
     @Test
     void whenNotExist() {
         double ab = 1.0;
