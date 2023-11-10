30f97870b7e1ab90557be8b6e5babf2b92bd3d93
andre <andreyklimenko@list.ru>
2023-11-11 00:29:00 +0300

    3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -4,17 +4,7 @@ public class Counter {
     public static int sum(int start, int finish) {
         int sum = 0;
         for (int i = start; i <= finish; i++) {
-            sum = sum + i;
-        }
-        return sum;
-    }
-
-    public static int sumByEven(int start, int finish) {
-        int sum = 0;
-        for (int i = start; i <= finish; i++) {
-            if (i % 2 == 0) {
-                sum = sum + i;
-            }
+            sum += i;
         }
         return sum;
     }
@@ -23,9 +13,5 @@ public class Counter {
         System.out.println(sum(0, 10));
         System.out.println(sum(3, 8));
         System.out.println(sum(1, 1));
-
-        System.out.println(sumByEven(0, 10));
-        System.out.println(sumByEven(3, 8));
-        System.out.println(sumByEven(1, 1));
     }
 }
