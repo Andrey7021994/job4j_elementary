70cb4457803d4fc570cf5ad90889328bc3038389
andre <andreyklimenko@list.ru>
2023-11-16 22:00:43 +0300

    3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -4,10 +4,12 @@ public class CheckPrimeNumber {
     public static boolean check(int number) {
         boolean prime = true;
         for (int index = 2; index <= number; index++) {
-            if  (number % index == 0 && number % 2 != 0) {
+            if  (number % index == 0) {
+                prime = false;
                 break;
             }
         }
-        return true;
+        return prime;
     }
 }
+
