9caf117e5bd66375092d14a56748a3392a6bcc69
andre <andreyklimenko@list.ru>
2023-11-21 00:29:39 +0300

    3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -2,12 +2,6 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        short[] ages = new short[10];
-        String[] surnames = new String[100500];
-        float[] prices = new float[40];
-        System.out.println("Размер массива равен: " + ages.length);
-        System.out.println("Размер массива равен: " + surnames.length);
-        System.out.println("Размер массива равен: " + prices.length);
         String[] names = new String[4];
         names[0] = "Fox";
         names[1] = "Wolf";
