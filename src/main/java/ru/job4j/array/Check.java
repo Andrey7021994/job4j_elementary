e0fb3eb83556efe4d137168bad58d017d71c110d
andre <andreyklimenko@list.ru>
2023-11-26 22:29:08 +0300

    3.3. Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/array/Check.java
@@ -0,0 +1,15 @@
+package ru.job4j.array;
+
+public class Check {
+    public static boolean mono(boolean[] data) {
+        boolean result = true;
+        for (int index = 1; index < data.length; index++) {
+            boolean temp = data[0];
+            if (data[index] != temp) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
