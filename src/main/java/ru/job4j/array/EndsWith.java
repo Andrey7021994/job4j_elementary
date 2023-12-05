eb30c5e3dd4b10440db41dbb06a553eedc09d00b
andre <andreyklimenko@list.ru>
2023-12-05 22:47:06 +0300

    3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/array/EndsWith.java
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -2,16 +2,11 @@ package ru.job4j.array;
 
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
-        boolean result = true;
-        if (word.length < post.length) {
-            return false;
-        } else {
-            for (int i = 0; i < post.length; i--) {
-                if (word[word.length - 1] != post[post.length - 1]) {
+            for (int i = 0; i < post.length; i++) {
+                if (word[word.length - post.length + i] != post[i]) {
                     return false;
                 }
             }
-        }
-        return result;
+        return true;
     }
 }
