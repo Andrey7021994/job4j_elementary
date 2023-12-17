3c3cfe476f9f9a2a975f597d5fa0e06ec56cdc5c
andre <andreyklimenko@list.ru>
2023-12-17 23:58:28 +0300

    3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -11,34 +11,4 @@ public class MatrixCheck {
         }
         return result;
     }
-
-    public static boolean monoVertical(char[][] board, int column) {
-        boolean result = true;
-        for (int i = 0; i < board[column].length; i++) {
-            if (board[i][column] != 'X') {
-                result = false;
-                break;
-            }
-        }
-        return result;
-    }
-
-    public static char[] extractDiagonal(char[][] board) {
-        char[] result = new char[board.length];
-        for (int i = 0; i < board.length; i++) {
-            result[i] = board[i][i];
-        }
-        return result;
-    }
-
-    public static boolean isWin(char[][] board) {
-        boolean result = false;
-        for (int i = 0; i < board.length; i++) {
-            if (board[i][i] == 'X' && (monoHorizontal(board, i) || monoVertical(board, i))) {
-                result = true;
-                break;
-            }
-        }
-        return result;
-    }
 }
